75a4fbd21ef95641525dcaaa81896d2194a244b0
jacksonvolante <jvolante@globusmedical.com>
2021-12-17 15:49:31 -0500

    Add languages to ensure installed

--- lua/settings/treesitter.lua
+++ lua/settings/treesitter.lua
@@ -9,7 +9,7 @@ parser_configs.norg = {
 }
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'cpp', 'c', 'rust', 'json', 'json5', 'cmake', 'bash' },
+  ensure_installed = { 'cpp', 'c', 'rust', 'json', 'json5', 'cmake', 'bash', 'lua', 'vim' },
   highlight = {
     enable = true
   }
