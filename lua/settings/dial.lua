32ce0510b3c6165315104e334913e41daa6bdf2c
jacksonvolante <jvolante@globusmedical.com>
2022-01-11 17:14:17 -0500

    toggle for bools

--- lua/settings/dial.lua
+++ lua/settings/dial.lua
@@ -1,3 +1,12 @@
+local dial = require("dial")
+
+dial.augends["custom#boolean"] = dial.common.enum_cyclic {
+  name = "boolean",
+  strlist = {"true", "false"}
+}
+
+table.insert(dial.config.searchlist.normal, "custom#boolean")
+
 local util = require('utilities')
 local map = util.map
 
