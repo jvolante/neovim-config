ddaefede499b0ccfda9d67b8f698d1ea70dc81c9
jacksonvolante <jvolante@globusmedical.com>
2022-01-12 11:15:06 -0500

    change keybind to tab

--- lua/settings/substitute.lua
+++ lua/settings/substitute.lua
@@ -3,7 +3,7 @@ require("substitute").setup()
 local util = require('utilities')
 local noremap = util.noremap
 
-noremap("n", "r", "<cmd>lua require('substitute').operator()<cr>")
-noremap("n", "rr", "<cmd>lua require('substitute').line()<cr>")
-noremap("n", "R", "<cmd>lua require('substitute').eol()<cr>")
-noremap("x", "r", "<cmd>lua require('substitute').visual()<cr>")
+noremap("n", "<tab>", "<cmd>lua require('substitute').operator()<cr>")
+noremap("n", "<tab><tab>", "<cmd>lua require('substitute').line()<cr>")
+noremap("n", "<s-tab>", "<cmd>lua require('substitute').eol()<cr>")
+noremap("x", "<tab>", "<cmd>lua require('substitute').visual()<cr>")
