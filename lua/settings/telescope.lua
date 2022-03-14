4d53a1d58a2e99710570f4d01a5d0e1fa6e57d81
jacksonvolante <jvolante@globusmedical.com>
2022-03-14 16:50:29 -0400

    Tweaks to telescop, add keybind for open buffers

--- lua/settings/telescope.lua
+++ lua/settings/telescope.lua
@@ -1,7 +1,8 @@
 local actions = require('telescope.actions')
 local config = require('telescope.config')
 
-vim.keymap.set('n', '<c-p>', function() require('telescope.builtin').find_files({ previewer=false }) end)
+vim.keymap.set('n', '<c-p>', require('telescope.builtin').find_files)
+vim.keymap.set('n', 'gB', require('telescope.builtin').buffers)
 vim.keymap.set('n', '<leader>/', require('telescope.builtin').live_grep)
 
 require('telescope').load_extension('yabs')
@@ -13,6 +14,17 @@ require('telescope').setup{
         ['<C-k>'] = actions.move_selection_previous,
         ['<ESC>'] = actions.close
       }
+    },
+    prompt_prefix = ' 🔍 ',
+  },
+  pickers = {
+    find_files = {
+      previewer = false,
+      theme = 'ivy',
+    },
+    buffers = {
+      previewer = false,
+      theme = 'ivy',
     }
   }
 }
