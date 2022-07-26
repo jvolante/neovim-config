eb8d6a5bf3e426db8d9faffa2f417738e44b13b4
BASE-OS-CHANGE-\Admin <bmathur@globusmedical.com>
2022-07-26 17:52:53 -0400

    Remove broken treesitter parser, add commands to toggle lsp lines

--- lua/settings/cmp.lua
+++ lua/settings/cmp.lua
@@ -209,3 +209,19 @@ require('lsp_lines').setup()
 vim.diagnostic.config {
   virtual_text = false,
 }
+
+vim.api.nvim_create_user_command('DlinesOff',
+  function()
+    vim.diagnostic.config {
+      virtual_text = true,
+      virtual_lines = false,
+    }
+  end, {})
+
+vim.api.nvim_create_user_command('DlinesOn',
+  function()
+    vim.diagnostic.config {
+      virtual_text = false,
+      virtual_lines = true,
+    }
+  end, {})
